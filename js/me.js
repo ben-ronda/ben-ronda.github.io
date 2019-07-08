7aa6a750f712bb2b0c54277b27d6bca6335981fd
Ben Ronda <bronda95@comcast.net>
2019-07-07 22:15:24 -0700

    add a me object for storing details like name, info/bio, and skills

---
+++ js/me.js
@@ -0,0 +1,16 @@
+const me = {
+  firstName: 'Ben',
+  lastName: 'Ronda',
+  about: `My name is Ben! I am a web developer in the Portland, OR area. I attended code school at <a href="http://www.epicodus.com/">Epicodus</a> in Portland. I enjoy coding, gaming, hiking/backpacking, and photography. I also love Star Wars with a passion.`,
+  skills: [
+    'HTML',
+    'CSS',
+    'PHP',
+    'Drupal',
+    'WordPress',
+    'JavaScript',
+    'Angular2',
+    'EmberJS',
+    'Python'
+  ]
+}
